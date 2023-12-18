ff20d4a78e66ae85677d9bfc23a668240efcc740
am230 <111672334+am230@users.noreply.github.com>
2023-12-19 07:53:11 +0900

    Update sqlitetable.py

--- omuserver/extension/table/adapters/sqlitetable.py
+++ omuserver/extension/table/adapters/sqlitetable.py
@@ -1,6 +1,6 @@
 import sqlite3
 from pathlib import Path
-from typing import Dict, List
+from typing import Dict
 
 from .tableadapter import Json, TableAdapter, json
 
@@ -11,8 +11,14 @@ class SqliteTableAdapter(TableAdapter):
         self._conn = sqlite3.connect(str(path / "data.db"))
         self._table = "data"
         self._conn.execute(
-            f"CREATE TABLE IF NOT EXISTS {self._table} (key TEXT PRIMARY KEY, value BLOB)"
+            # index, key, value
+            f"CREATE TABLE IF NOT EXISTS {self._table} ("
+            "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+            "key TEXT UNIQUE,"
+            "value TEXT"
+            ")"
         )
+        self._conn.commit()
 
     @classmethod
     def create(cls, path: Path) -> "TableAdapter":
@@ -33,9 +39,9 @@ class SqliteTableAdapter(TableAdapter):
             return None
         return json.loads(row[0])
 
-    async def get_all(self, keys: List[str]) -> Dict[str, Json]:
+    async def get_all(self, keys: list[str]) -> Dict[str, Json]:
         cursor = self._conn.execute(
-            f"SELECT key, value FROM {self._table} WHERE key IN ({','.join(['?'] * len(keys))})",
+            f"SELECT key, value FROM {self._table} WHERE key IN ({','.join('?' for _ in keys)})",
             keys,
         )
         rows = cursor.fetchall()
@@ -50,15 +56,15 @@ class SqliteTableAdapter(TableAdapter):
     async def set_all(self, items: Dict[str, Json]) -> None:
         self._conn.executemany(
             f"INSERT OR REPLACE INTO {self._table} (key, value) VALUES (?, ?)",
-            [(key, json.dumps(value)) for key, value in items.items()],
+            ((key, json.dumps(value)) for key, value in items.items()),
         )
 
     async def remove(self, key: str) -> None:
         self._conn.execute(f"DELETE FROM {self._table} WHERE key = ?", (key,))
 
-    async def remove_all(self, keys: List[str]) -> None:
+    async def remove_all(self, keys: list[str]) -> None:
         self._conn.execute(
-            f"DELETE FROM {self._table} WHERE key IN ({','.join(['?'] * len(keys))})",
+            f"DELETE FROM {self._table} WHERE key IN ({','.join('?' for _ in keys)})",
             keys,
         )
 
@@ -79,19 +85,19 @@ class SqliteTableAdapter(TableAdapter):
         return {row[0]: json.loads(row[1]) for row in rows}
 
     async def first(self) -> str | None:
-        cursor = self._conn.execute(
+        _cursor = self._conn.execute(
             f"SELECT key FROM {self._table} ORDER BY key LIMIT 1"
         )
-        row = cursor.fetchone()
+        row = _cursor.fetchone()
         if row is None:
             return None
         return row[0]
 
     async def last(self) -> str | None:
-        cursor = self._conn.execute(
+        _cursor = self._conn.execute(
             f"SELECT key FROM {self._table} ORDER BY key DESC LIMIT 1"
         )
-        row = cursor.fetchone()
+        row = _cursor.fetchone()
         if row is None:
             return None
         return row[0]
@@ -100,6 +106,8 @@ class SqliteTableAdapter(TableAdapter):
         self._conn.execute(f"DELETE FROM {self._table}")
 
     async def size(self) -> int:
-        cursor = self._conn.execute(f"SELECT COUNT(*) FROM {self._table}")
-        row = cursor.fetchone()
+        _cursor = self._conn.execute(f"SELECT COUNT(*) FROM {self._table}")
+        row = _cursor.fetchone()
+        if row is None:
+            return 0
         return row[0]
