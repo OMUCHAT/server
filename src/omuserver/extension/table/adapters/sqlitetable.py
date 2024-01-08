01093641af000e4293634fb07a7538a155209096
am230 <111672334+am230@users.noreply.github.com>
2024-01-08 21:32:44 +0900

    Update sqlitetable.py

--- src/omuserver/extension/table/adapters/sqlitetable.py
+++ src/omuserver/extension/table/adapters/sqlitetable.py
@@ -11,10 +11,9 @@ class SqliteTableAdapter(TableAdapter):
     def __init__(self, path: Path) -> None:
         self._path = path
         self._conn = sqlite3.connect(str(path / "data.db"))
-        self._table = "data"
         self._conn.execute(
             # index, key, value
-            f"CREATE TABLE IF NOT EXISTS {self._table} ("
+            "CREATE TABLE IF NOT EXISTS data ("
             "id INTEGER PRIMARY KEY AUTOINCREMENT,"
             "key TEXT UNIQUE,"
             "value TEXT"
@@ -33,9 +32,7 @@ class SqliteTableAdapter(TableAdapter):
         pass
 
     async def get(self, key: str) -> Json | None:
-        cursor = self._conn.execute(
-            f"SELECT value FROM {self._table} WHERE key = ?", (key,)
-        )
+        cursor = self._conn.execute("SELECT value FROM data WHERE key = ?", (key,))
         row = cursor.fetchone()
         if row is None:
             return None
@@ -43,7 +40,7 @@ class SqliteTableAdapter(TableAdapter):
 
     async def get_all(self, keys: list[str]) -> Dict[str, Json]:
         cursor = self._conn.execute(
-            f"SELECT key, value FROM {self._table} WHERE key IN ({','.join('?' for _ in keys)})",
+            f"SELECT key, value FROM data WHERE key IN ({','.join('?' for _ in keys)})",
             keys,
         )
         rows = cursor.fetchall()
@@ -51,22 +48,22 @@ class SqliteTableAdapter(TableAdapter):
 
     async def set(self, key: str, value: Json) -> None:
         self._conn.execute(
-            f"INSERT OR REPLACE INTO {self._table} (key, value) VALUES (?, ?)",
+            "INSERT OR REPLACE INTO data (key, value) VALUES (?, ?)",
             (key, json.dumps(value)),
         )
 
     async def set_all(self, items: Dict[str, Json]) -> None:
         self._conn.executemany(
-            f"INSERT OR REPLACE INTO {self._table} (key, value) VALUES (?, ?)",
+            "INSERT OR REPLACE INTO data (key, value) VALUES (?, ?)",
             ((key, json.dumps(value)) for key, value in items.items()),
         )
 
     async def remove(self, key: str) -> None:
-        self._conn.execute(f"DELETE FROM {self._table} WHERE key = ?", (key,))
+        self._conn.execute("DELETE FROM data WHERE key = ?", (key,))
 
     async def remove_all(self, keys: list[str]) -> None:
         self._conn.execute(
-            f"DELETE FROM {self._table} WHERE key IN ({','.join('?' for _ in keys)})",
+            f"DELETE FROM data WHERE key IN ({','.join('?' for _ in keys)})",
             keys,
         )
 
@@ -80,41 +77,37 @@ class SqliteTableAdapter(TableAdapter):
         items = {}
         if before is not None:
             _cursor = self._conn.execute(
-                f"SELECT key, value FROM {self._table} WHERE key <= ? ORDER BY id DESC LIMIT ?",
+                "SELECT key, value FROM data WHERE key <= ? ORDER BY id DESC LIMIT ?",
                 (cursor, before),
             )
             items.update({row[0]: json.loads(row[1]) for row in _cursor.fetchall()})
         if after is not None:
             _cursor = self._conn.execute(
-                f"SELECT key, value FROM {self._table} WHERE key >= ? ORDER BY id ASC LIMIT ?",
+                "SELECT key, value FROM data WHERE key >= ? ORDER BY id ASC LIMIT ?",
                 (cursor, after),
             )
             items.update({row[0]: json.loads(row[1]) for row in _cursor.fetchall()})
         return items
 
     async def first(self) -> str | None:
-        _cursor = self._conn.execute(
-            f"SELECT key FROM {self._table} ORDER BY id LIMIT 1"
-        )
+        _cursor = self._conn.execute("SELECT key FROM data ORDER BY id LIMIT 1")
         row = _cursor.fetchone()
         if row is None:
             return None
         return row[0]
 
     async def last(self) -> str | None:
-        _cursor = self._conn.execute(
-            f"SELECT key FROM {self._table} ORDER BY id DESC LIMIT 1"
-        )
+        _cursor = self._conn.execute("SELECT key FROM data ORDER BY id DESC LIMIT 1")
         row = _cursor.fetchone()
         if row is None:
             return None
         return row[0]
 
     async def clear(self) -> None:
-        self._conn.execute(f"DELETE FROM {self._table}")
+        self._conn.execute("DELETE FROM data")
 
     async def size(self) -> int:
-        _cursor = self._conn.execute(f"SELECT COUNT(*) FROM {self._table}")
+        _cursor = self._conn.execute("SELECT COUNT(*) FROM data")
         row = _cursor.fetchone()
         if row is None:
             return 0
